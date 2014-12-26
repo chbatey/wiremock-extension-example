f2dd79dc2c3464df9304fec997525bccecd9cf9d
Christopher Batey <christopher.batey@gmail.com>
2014-12-26 17:35:51 +0000

    adding example that copies batey headers

--- src/main/java/info/batey/wiremock/extension/CopiesBateyHeaders.java
+++ src/main/java/info/batey/wiremock/extension/CopiesBateyHeaders.java
@@ -1,4 +1,31 @@
 package info.batey.wiremock.extension;
 
-public class CopiesBateyHeaders implements WiremockEx {
+import com.github.tomakehurst.wiremock.http.*;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class CopiesBateyHeaders implements WiremockExtension {
+    @Override
+    public ResponseDefinition filter(Request request, ResponseDefinition responseDefinition) {
+        Collection<HttpHeader> originalResponseHeaders = responseDefinition.getHeaders().all();
+        Set<String> allHeaderKeys = request.getAllHeaderKeys();
+        for (String headerName : allHeaderKeys) {
+            if (headerName.startsWith("Batey")) {
+                originalResponseHeaders.add(new HttpHeader(headerName, request.getHeader(headerName)));
+            }
+        }
+        responseDefinition.setHeaders(new HttpHeaders(originalResponseHeaders));
+        return responseDefinition;
+    }
+
+    @Override
+    public String getName() {
+        return "Batey Header Copier";
+    }
+
+    @Override
+    public boolean isGlobal() {
+        return true;
+    }
 }
